66e5efe5baaa134c78a47f78c4e83bea3cfd03a9
Werneke <ew774515@ohio.edu>
2020-01-22 10:16:54 -0500

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ew774515_MIS4800.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ew774515_MIS4800.Startup))]
+namespace ew774515_MIS4800
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
