9d236b38cb24731ab9b7b4b5b20dd8a9e9343eb6
Eleanor Werneke <ew774515@ohio.edu>
2020-01-27 10:34:04 -0500

    made customer

---
+++ Models/customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ew774515_MIS4800.Models
+{
+    public class customer
+    {
+        public int customerID { get; set; }
+
+      //prop tab tab will get you this
+        public string customerLastName { get; set; }
+        public string customerFirstName { get; set; }
+        public string email { get; set; }
+        public string phone{ get; set; }
+
+        public DateTime customerSince { get; set; }
+    }
+}
