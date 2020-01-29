e65417d74dff4b73f7fd128344d8fe8a5f691491
Eleanor Werneke <ew774515@ohio.edu>
2020-01-29 10:35:57 -0500

    After class 1/29

---
+++ DAL/MIS4200Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using ew774515_MIS4800.Models;
+
+namespace ew774515_MIS4800.DAL
+{
+    public class MIS4200Context: DbContext
+    {
+        public MIS4200Context() : base("name=DefaultConnection")
+        {
+
+        }
+        public DbSet<customer> Customers { get; set; }
+        public DbSet<orders> Orders { get; set; }
+    }
+}
